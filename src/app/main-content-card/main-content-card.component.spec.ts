4d9f1cb1b5e9052da6972f91fe7ec7d8a484cd3e
Plastikaweb <info@plastikaweb.com>
2017-03-30 12:54:11 +0200

    feat(main content card component):

---
+++ src/app/main-content-card/main-content-card.component.spec.ts
@@ -0,0 +1,42 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CovalentCoreModule } from '@covalent/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
+
+import { MainContentCardComponent } from './main-content-card.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { WorksComponent } from '../works/works.component';
+import { ContactComponent } from '../contact/contact.component';
+import { WhoComponent } from '../who/who.component';
+
+describe('MainContentCardComponent', () => {
+  let component: MainContentCardComponent;
+  let fixture: ComponentFixture<MainContentCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CovalentCoreModule.forRoot(),
+        RouterModule,
+        AppRoutingModule
+      ],
+      providers: [ { provide: APP_BASE_HREF, useValue: '/' }, ChangeDetectorRef],
+      declarations: [ MainContentCardComponent,
+        WorksComponent,
+        WhoComponent,
+        ContactComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MainContentCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/main-content-card/main-content-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-content-card',
+  templateUrl: './main-content-card.component.html',
+  styleUrls: ['./main-content-card.component.scss']
+})
+export class MainContentCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
