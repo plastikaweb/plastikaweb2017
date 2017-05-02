4206fc97bd3aa684cd9a767a8468afb2ea226f84
Carlos Matheu <info@plastikaweb.com>
2017-05-02 15:19:44 +0200

    who component - skills test dummy data

--- src/app/who/who.component.ts
+++ src/app/who/who.component.ts
@@ -1,24 +1,49 @@
-import { Component } from '@angular/core';
-import {single, multi} from '../../data/data';
+import { Component, OnInit } from '@angular/core';
+import {
+  TdDataTableService,
+  TdDataTableSortingOrder,
+  ITdDataTableColumn,
+  ITdDataTableSortChangeEvent
+} from '@covalent/core';
 
 @Component({
   selector: 'app-who',
   templateUrl: './who.component.html',
-  styleUrls: ['./who.component.scss']
+  styleUrls: [ './who.component.scss' ]
 })
-export class WhoComponent {
-  single: any[];
-  multi: any[];
-  view: any[] = [400, 400];
-  colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
-  };
+export class WhoComponent implements OnInit {
+  data: any[] = [
+    { skill: 'angular', years: '1', proficiency: 70 },
+    { skill: 'javascript', years: '7', proficiency: 85 },
+    { skill: 'CSS3', years: '5', proficiency: 80 },
+    { skill: 'Sass', years: '3', proficiency: 70 },
+    { skill: 'HTML5', years: '7', proficiency: 70 }
+  ];
+  columns: ITdDataTableColumn[] = [
+    { name: 'skill', label: 'skill', tooltip: 'skill / technology' },
+    { name: 'years', label: 'years', numeric: true },
+    { name: 'proficiency', label: 'proficiency', numeric: true },
+  ];
 
-  constructor() {
-    Object.assign(this, {single, multi});
+  filteredData: any[] = this.data;
+  sortBy = 'skill';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+  constructor(private dataTableService: TdDataTableService) {}
+
+  ngOnInit(): void {
+    this.filter();
   }
 
-  onSelect(event) {
-    console.log(event);
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  filter(): void {
+    let newData: any[] = this.data;
+    newData = this.dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    this.filteredData = newData;
   }
 }
