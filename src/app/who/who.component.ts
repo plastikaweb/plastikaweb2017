d6a55ae37b68911b650880574cf1909fef6f4718
Carlos <carlos@iMac-de-Ramon.local>
2017-05-05 17:05:25 +0200

    style(who): styling skills indicators with sass mixin and variables

--- src/app/who/who.component.ts
+++ src/app/who/who.component.ts
@@ -9,12 +9,12 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class WhoComponent implements OnInit {
   data: any[] = [
-    { skill: 'angular', years: '2', proficiency: 75, icon: 'angular.svg', color: 'rgba(255, 109, 113, 0.4)' },
-    { skill: 'javascript', years: '7', proficiency: 85, icon: 'javascript.svg', color: 'rgba(255, 227, 0, 0.4)' },
-    { skill: 'typescript', years: '1', proficiency: 75, icon: 'typescript.svg', color: 'rgba(132, 206, 255, 0.5)' },
-    { skill: 'CSS3', years: '5', proficiency: 75, icon: 'css3.svg', color: 'rgba(107, 134, 152, 0.5)' },
-    { skill: 'Sass', years: '3', proficiency: 70, icon: 'sass.svg', color: 'rgba(241, 184, 215, 0.5)' },
-    { skill: 'HTML5', years: '7', proficiency: 80, icon: 'html5.svg', color: 'rgba(253, 117, 59, 0.4)' }
+    { skill: 'angular', years: '2', proficiency: 75, icon: 'angular.svg', class: 'red'},
+    { skill: 'javascript', years: '7', proficiency: 85, icon: 'javascript.svg', class: 'yellow' },
+    { skill: 'typescript', years: '1', proficiency: 75, icon: 'typescript.svg', class: 'blue' },
+    { skill: 'CSS3', years: '5', proficiency: 75, icon: 'css3.svg', class: 'grey' },
+    { skill: 'Sass', years: '3', proficiency: 70, icon: 'sass.svg', class: 'pink' },
+    { skill: 'HTML5', years: '7', proficiency: 80, icon: 'html5.svg', class: 'orange' }
   ];
 
   constructor(private iconRegistry: MdIconRegistry,
