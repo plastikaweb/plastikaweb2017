53548b8fc8ecc035fcb5fe5493b2e76d9e892bf7
Carlos <carlos@iMac-de-Ramon.local>
2017-05-10 18:27:46 +0200

    feat(contact): contact section
    closes #6

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MdIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  constructor(private iconRegistry: MdIconRegistry,
+              private sanitizer: DomSanitizer) {
+  }
 
+  ngOnInit() {
+    this.iconRegistry.addSvgIcon(
+      'linkedin',
+      this.sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/linkedin.svg`));
+    this.iconRegistry.addSvgIcon(
+      'facebook',
+      this.sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/facebook.svg`));
+    this.iconRegistry.addSvgIcon(
+      'twitter',
+      this.sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/twitter.svg`));
+  }
 }
