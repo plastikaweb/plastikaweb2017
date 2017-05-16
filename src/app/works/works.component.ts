7f0f119c88e03b43cd4b4250148b5bc6bfb25eff
Carlos <carlos@iMac-de-Ramon.local>
2017-05-16 10:52:13 +0200

    feat(works images and temp component):

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { projects } from '../../../db/data/projects';
 
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
-  styleUrls: ['./works.component.scss']
+  styleUrls: [ './works.component.scss' ]
 })
 export class WorksComponent {
+  data: any[] = projects;
+
+  getMainImage(name) {
+    return `assets/projects/${name}/${name}-1.png`;
+  }
+  getMainTechImage(name) {
+  return `assets/icons/${name}.svg`;
+}
 
 }
