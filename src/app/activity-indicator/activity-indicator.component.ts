4caba459617582b2591a3f928c884c5616188038
Carlos <carlos@iMac-de-Ramon.local>
2017-05-19 16:31:09 +0200

    refactor(activity-indicator): deleted activity-indicator, until implementation in the future

--- src/app/activity-indicator/activity-indicator.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-activity-indicator',
-  templateUrl: './activity-indicator.component.html'
-})
-export class ActivityIndicatorComponent {
-  @Input() pending = false;
-}
