4fe6f73099446ff7dac48a48a0645a0a9ae64c93
Carlos <carlos@iMac-de-Ramon.local>
2017-05-29 18:23:19 +0200

    ci(travis): fix firebase config name file to output

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,11 +2,11 @@
 
 set -ev
 
-# Setup Firebase and Translation config files
+# Setup Firebase config
 cat src/config/firebase.config.ts.sample \
     | sed "s/FIREBASE-APP-API-KEY/${FIREBASE_APP_API_KEY}/g" \
     | sed "s/FIREBASE-APP/${FIREBASE_APP}/g" \
     | sed "s/FIREBASE-APP-MESSAGING-SENDER-ID/${FIREBASE_APP_MESSAGING_SENDER_ID}/g" \
-    | tee src/config/firebase.ts &>/dev/null
+    | tee src/config/firebase.config.ts &>/dev/null
 
 exit 0
