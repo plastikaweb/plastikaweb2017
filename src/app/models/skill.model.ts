43cc8a899462d692ed5d2466dbfff8749312b69a
Carlos Matheu <info@plastikaweb.com>
2017-06-08 11:19:17 +0200

    fix(ISkill): Property 'legend' and 'description' does not exist on type 'ISkill
    closes #70

--- src/app/models/skill.model.ts
+++ src/app/models/skill.model.ts
@@ -1,10 +1,14 @@
+import { ITranslation } from './work.model';
+
 export interface ISkill {
   name: string;
   order: number;
   active: boolean;
   class: string;
   icon: string;
+  legend: ITranslation;
+  description: ITranslation;
   proficiency: number;
   years: number;
-  slug: string;
+  slug: any;
 }
