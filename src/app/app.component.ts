9f3c7adf1f8b090790ec39609e2aa36cde223108
Carlos Matheu <info@plastikaweb.com>
2017-06-13 09:27:25 +0200

    feat(html[lang]): change attribute lang of html dynamically
    closes #86

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import { langConfig } from '../config/lang.config';
 import { LocalizeRouterService } from 'localize-router';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,33 @@ import { LocalizeRouterService } from 'localize-router';
 export class AppComponent implements OnInit {
 
   constructor(private translate: TranslateService,
-              private localize: LocalizeRouterService) {
+              private localize: LocalizeRouterService,
+              @Inject(DOCUMENT) private document) {
   }
 
   ngOnInit() {
     // get app languages and set current language
     this.translate.addLangs(langConfig.languages);
     this.translate.setDefaultLang(langConfig.defaultLang);
+    this.changeLangAttr(this.translate.currentLang);
   }
 
+  /**
+   * update app lang
+   * @param lang
+   */
   onChangeLang(lang) {
     this.localize.changeLanguage(lang);
+    this.changeLangAttr(lang);
+  }
+
+  /**
+   * Change lang attr for head tag on change app lang
+   * @param lang
+   */
+  changeLangAttr(lang) {
+    this.document.getElementsByTagName('html')[ 0 ]
+      .setAttribute('lang', lang);
   }
 
 }
