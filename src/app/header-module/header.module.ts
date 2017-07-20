0cdc09db5b2ef0a19d565119f83037af6a57fe8e
Carlos Matheu Armengol <info@plastikaweb.com>
2017-07-20 11:16:08 +0200

    fix(header): home header btn error
    404 page on change lang when clicking home button fix
    
    closes #137

--- src/app/header-module/header.module.ts
+++ src/app/header-module/header.module.ts
@@ -1,6 +1,7 @@
 import { Angulartics2Module } from 'angulartics2';
 import { CommonModule } from '@angular/common';
 import { CovalentMenuModule } from '@covalent/core';
+import { LocalizeRouterModule } from 'localize-router';
 import { NgModule } from '@angular/core';
 import { MdButtonModule, MdIconModule, MdListModule, MdMenuModule, MdTooltipModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
@@ -14,6 +15,7 @@ import { LanguageMenuComponent } from './language-menu/language-menu.component';
     Angulartics2Module,
     CommonModule,
     CovalentMenuModule,
+    LocalizeRouterModule,
     MdButtonModule,
     MdIconModule,
     MdListModule,
