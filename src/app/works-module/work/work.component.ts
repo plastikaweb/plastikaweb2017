26ad7f9efe86aa875de38df4502ee5745fb49245
Carlos Matheu Armengol <info@plastikaweb.com>
2017-09-24 12:23:49 +0200

    fix(product): fix product lazy load of images on scrolling both on product and products view
    closes #100

--- src/app/works-module/work/work.component.ts
+++ src/app/works-module/work/work.component.ts
@@ -1,12 +1,12 @@
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BreadcrumbService } from 'ng2-breadcrumb/bundles/components/breadcrumbService';
-import { ChangeDetectionStrategy, Component, HostBinding, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { TranslateService } from '@ngx-translate/core';
+import { BreadcrumbService } from 'ng2-breadcrumb/bundles/components/breadcrumbService';
+import { Observable } from 'rxjs/Observable';
 
 import { fadeAnimation } from '../../animations/fade.animation';
-import { ImagesService } from '../../shared/images-service/images.service';
 import { ITranslation, IWork } from '../../models/work.model';
+import { ImagesService } from '../../shared/images-service/images.service';
 
 @Component({
   selector: 'app-work',
@@ -19,15 +19,21 @@ export class WorkComponent implements OnInit {
   @HostBinding('style.display') display = 'block';
   work$: Observable<IWork>;
   slug;
-  offset = 100;
+  myScrollContainer;
+  offset = 0;
   imagesService = ImagesService;
 
   constructor(private route: ActivatedRoute,
               private breadcrumbService: BreadcrumbService,
+              private elmRef: ElementRef,
+              private renderer: Renderer2,
               private translate: TranslateService) {
   }
 
   ngOnInit() {
+    this.myScrollContainer = this.renderer.parentNode(
+      this.elmRef.nativeElement.parentNode.parentNode
+    );
     this.work$ = this.route.data.map(data => data.work[ 0 ]);
     // TODO check if the named route for work is already added
     // or it is the first time (the loading has gone directly to work detail)
--- src/app/works-module/works.component.ts
+++ src/app/works-module/works.component.ts
@@ -1,16 +1,16 @@
-import { ActivatedRoute } from '@angular/router';
 import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, OnInit, Renderer2 } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
 
 import { fadeAnimation } from '../animations/fade.animation';
-import { ImagesService } from '../shared/images-service/images.service';
 import { ITranslation, IWork } from '../models/work.model';
+import { ImagesService } from '../shared/images-service/images.service';
 
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
-  animations: [ fadeAnimation ],
+  animations: [fadeAnimation],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WorksComponent implements OnInit {
@@ -19,39 +19,39 @@ export class WorksComponent implements OnInit {
 
   works$: Observable<IWork[]>;
   allTags$: Observable<string[]>;
-  offset = 100;
+  offset = 0;
   myScrollContainer;
   imagesService = ImagesService;
   tagsFilter: string[] = [];
   sendTagSelection;
 
-  constructor(private route: ActivatedRoute,
-              private elmRef: ElementRef,
-              private renderer: Renderer2,
-              private translate: TranslateService) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private elmRef: ElementRef,
+    private renderer: Renderer2,
+    private translate: TranslateService
+  ) {}
 
   ngOnInit() {
-    this.myScrollContainer = this.renderer
-      .parentNode(this.elmRef.nativeElement.parentNode);
-
-    this.works$ = this.route.data.map(data => data.works[ 0 ]);
-    this.allTags$ = this.route.data.map(data => data.works[ 1 ]);
+    this.myScrollContainer = this.renderer.parentNode(
+      this.elmRef.nativeElement.parentNode.parentNode
+    );
+    this.works$ = this.route.data.map(data => data.works[0]);
+    this.allTags$ = this.route.data.map(data => data.works[1]);
     // TODO find fix
     this.translate.reloadLang(this.translate.currentLang);
   }
 
   doFilter(tags: string[]) {
-    this.tagsFilter = [ ...tags ];
+    this.tagsFilter = [...tags];
     this.sendTagSelection = '';
   }
 
   getRemoteTranslation(item: ITranslation) {
-    return item[ this.translate.currentLang ];
+    return item[this.translate.currentLang];
   }
 
   tagSelection(e: string) {
     this.sendTagSelection = e;
   }
-
 }
